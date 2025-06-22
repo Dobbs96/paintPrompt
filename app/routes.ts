b31a2271fef290682799a590da06144b0735bb36
k-onate <kevinonatealt@gmail.com>
2025-06-22 12:31:56 -0400

    Added welcome page

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default 
+[
+    index("routes/welcomepage.tsx"),
+    //route("welcomepage","routes/welcomepage.tsx"),
+] satisfies RouteConfig;
