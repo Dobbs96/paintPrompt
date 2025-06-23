1d84ce33fa4c17bc11af63886ea56f74456465d2
James Dobbs <84473747+Dobbs96@users.noreply.github.com>
2025-06-22 20:42:28 -0400

    Update routes.ts
    Need fixed merge didn't go right

--- app/routes.ts
+++ app/routes.ts
@@ -7,15 +7,7 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-<<<<<<< HEAD
-    index("routes/welcomepage.tsx"),
-    route("/home", "./home/home.tsx"),
-=======
-  //removing the comment marker below gives me an error, im not sure if it's an issue on my end - kevin
-  //index("routes/home.tsx"),
-  route("/", "routes/welcomepage.tsx"),
-  route("/home", "./home/Home.tsx"),
-  route("/login","routes/login.tsx"),
-
->>>>>>> main
+    route("/", "routes/welcomepage.tsx"),
+    route("/home", "./home/Home.tsx"),
+    route("/login", "routes/login.tsx"),
 ] satisfies RouteConfig;
