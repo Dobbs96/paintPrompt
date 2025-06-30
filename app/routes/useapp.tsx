a2cb762c54506cc30f936c85ccea869a81172e62
James Dobbs <84473747+Dobbs96@users.noreply.github.com>
2025-06-30 16:21:42 -0400

    created how to use app adn keeping fotter the same

--- app/routes/useapp.tsx
+++ app/routes/useapp.tsx
@@ -2,5 +2,14 @@ import { useNavigate } from "react-router";
 
 export default function UseAppPage() {
     const navigate = useNavigate();
-    return <p>How to use app HelpPage</p>;
+    return (
+        <div>
+            {/* Footer */}
+            <div className="bg-gray-100 text-center py-6 text-sm text-gray-600 flex flex-row justify-center items-center gap-x-8 text-center flex-wrap">
+                <p>© 2023 Paint Prompt. All rights reserved.</p>
+                <p>Follow us on social media for updates!</p>
+                <p>Privacy Policy | Terms of Service | Help Center</p>
+            </div>
+        </div>
+    );
 }
