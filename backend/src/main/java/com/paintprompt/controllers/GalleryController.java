bcd1c5ef441eb87feeefc7899783cde24999fdd0
Devorex <devorex.minn10@myhunter.cuny.edu>
2025-07-17 16:18:26 -0400

    add GalleryController and GalleryItem model to backend

---
+++ backend/src/main/java/com/paintprompt/controllers/GalleryController.java
@@ -0,0 +1,25 @@
+package com.paintprompt.controllers;
+
+import com.paintprompt.models.GalleryItem;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/gallery")
+@CrossOrigin(origins = "http://localhost:3000")
+public class GalleryController {
+
+    private final List<GalleryItem> gallery = new ArrayList<>();
+
+    @GetMapping
+    public List<GalleryItem> getGallery() {
+        return gallery;
+    }
+
+    @PostMapping
+    public void addArtwork(@RequestBody GalleryItem item) {
+        gallery.add(item);
+    }
+}
