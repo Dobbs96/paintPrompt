38f94f4542e248de4e49c5246dcba8991fc40b9b
James Dobbs <84473747+Dobbs96@users.noreply.github.com>
2025-07-19 18:17:21 -0400

    Relax TypeScript build strictness for Vercel

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcomepage from "./routes/welcomepage";
 import Home from "./routes/home";
 import Login from "./routes/login";
